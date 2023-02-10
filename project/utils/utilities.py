177fd7c0becaf0f0cd1af4f501d9cdb0d78e2aa0
Richard Scheiwe <richard.s@RICHARD-S-M1.local>
2023-02-10 00:37:12 -0500

    updated

--- project/utils/utilities.py
+++ project/utils/utilities.py
@@ -1,11 +1,11 @@
 import bcrypt
-# import http3
-import httpx
+import http3
+
+
 
 
 async def call_api(url: str):
-    # client = http3.AsyncClient()
-    client = httpx.AsyncClient()
+    client = http3.AsyncClient()
     r = await client.get(url)
     return r.json()
 
